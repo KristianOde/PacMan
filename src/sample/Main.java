72f16f3a26da2991fd0608501f16cc973ef4cd4b
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2020-02-25 12:46:50 +0100

    GridPane

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -16,6 +17,14 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         BorderPane pane = new BorderPane();
         GridPane gPane = new GridPane();
+        GameMap map = new GameMap();
+        gPane.setGridLinesVisible(true);
+        for(int i=0; i<=GameMap.cellArray.length(); i++ ) {
+
+        }
+        gPane.add(new Circle(50, 50, 50), 0, 0);
+        System.out.println(gPane.isGridLinesVisible());
+
         pane.setCenter(gPane);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Scene scene = new Scene(pane, 600, 600);
