cc6639d2848a22de0504ead78ae1d6f2484c5aa0
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2020-02-26 14:04:09 +0100

    gPane -> pane, pane -> bPane

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,36 +7,39 @@ import javafx.scene.Node;
 import javafx.geometry.HPos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.util.Arrays;
 
 public class Main extends Application {
-    BorderPane pane = new BorderPane();
-    GridPane gPane = new GridPane();
+    BorderPane bPane = new BorderPane();
+    Pane pane = new Pane();
     GameMap map = new GameMap();
     Creature pacMan = new PacMan();
     @Override
     public void start(Stage primaryStage) throws Exception {
         System.out.println(Arrays.deepToString(map.accessMapArray()));
         initializeGame();
-        pane.setStyle("-fx-background-color: black;");
-        pane.setCenter(gPane);
+        bPane.setStyle("-fx-background-color: black;");
+        bPane.setCenter(pane);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        gPane.setOnKeyPressed(e -> {
+        /*pane.setOnKeyPressed(e -> {
             switch (e.getCode()) {
                 case LEFT:
-                    //Node node = getNodeByRowColumnIndex(pacMan.getY(), pacMan.getX(), gPane);
+                    //Node node = getNodeByRowColumnIndex(pacMan.getY(), pacMan.getX(), pane);
                     pacMan.setXY(pacMan.getX()-1, pacMan.getY());
                     break;
                 case RIGHT:
-                    gPane.add(pacMan, pacMan.getX()+1, pacMan.getY());
+                    pane.add(pacMan, pacMan.getX()+1, pacMan.getY());
                     break;
             }
-        });
-        Scene scene = new Scene(pane);
+        });*/
+
+        Scene scene = new Scene(bPane);
         primaryStage.setTitle("PacMan");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
@@ -48,21 +51,21 @@ public class Main extends Application {
         for (int i=0; i<27;i++) {
             for (int j=0; j<21; j++) {
                 if (!map.accessMapArray()[i][j].getPassable()) {
-                    gPane.add(new Wall(), j, i);
+                    pane.add(new Wall(), j, i);
                 }
                 else if (map.accessMapArray()[i][j].getBigDot()) {
                     Dot bigDot = new Dot(true);
-                    gPane.add(bigDot, j, i);
+                    pane.add(bigDot, j, i);
                     GridPane.setHalignment(bigDot, HPos.CENTER);
                 }
                 else if (map.accessMapArray()[i][j].getDot()) {
                     Dot dot = new Dot(false);
-                    gPane.add(dot, j, i);
+                    pane.add(dot, j, i);
                     GridPane.setHalignment(dot, HPos.CENTER);
                 }
                 else if (map.accessMapArray()[i][j].getStarterArea()) {
                     pacMan.setXY(j, i);
-                    gPane.add(pacMan, pacMan.getX(), pacMan.getY());
+                    pane.add(pacMan, pacMan.getX(), pacMan.getY());
                     GridPane.setHalignment(pacMan, HPos.CENTER);
                 }
             }
