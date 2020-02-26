1fcfac4efd8d1f3b04cbc7ca4e4217dabfa9dd2f
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2020-02-26 15:37:51 +0100

    Animasjon

--- src/sample/Creature.java
+++ src/sample/Creature.java
@@ -1,11 +1,20 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 public class Creature extends Circle {
     protected int x, y;
     protected Direction dir;
-    public Creature() {}
+    protected Timeline animation;
+
+    public Creature() {
+        animation = new Timeline(new KeyFrame(Duration.millis(5), e -> move()));
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.play(); // Start animation
+    }
     public void setXY(int x, int y) {
         this.x = x; this.y = y;
     }
@@ -23,13 +32,15 @@ public class Creature extends Circle {
     public Direction getDirection() {
         return dir;
     }
-
     public void move() {
         if (this.getDirection() == Direction.LEFT) {
             this.setCenterX(this.getCenterX() - 1);
-        }
-        if (this.getDirection() == Direction.RIGHT) {
+        }else if (this.getDirection() == Direction.RIGHT) {
             this.setCenterX(this.getCenterX() + 1);
+        }else if (this.getDirection() == Direction.UP) {
+            this.setCenterY(this.getCenterY() - 1);
+        }else if (this.getDirection() == Direction.DOWN) {
+            this.setCenterY(this.getCenterY() + 1);
         }
     }
 }
