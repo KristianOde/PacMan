0c080b32cb2713e5ddca5d447ca770f8f7909f5c
Kristian <46893137+KristianUSN@users.noreply.github.com>
2020-02-26 14:17:56 +0100

    ok

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,42 +1,33 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.geometry.HPos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.util.Arrays;
 
 public class Main extends Application {
-    BorderPane pane = new BorderPane();
-    GridPane gPane = new GridPane();
+    BorderPane bPane = new BorderPane();
+    Pane pane = new Pane();
     GameMap map = new GameMap();
     Creature pacMan = new PacMan();
+    int gridRows = map.accessMapArray().length;
+    int gridColumns = map.accessMapArray()[0].length;
     @Override
     public void start(Stage primaryStage) throws Exception {
         System.out.println(Arrays.deepToString(map.accessMapArray()));
         initializeGame();
-        pane.setStyle("-fx-background-color: black;");
-        pane.setCenter(gPane);
+        bPane.setStyle("-fx-background-color: black;");
+        bPane.setCenter(pane);
+        System.out.println(gridRows " " + gridColumns);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        gPane.setOnKeyPressed(e -> {
-            switch (e.getCode()) {
-                case LEFT:
-                    //Node node = getNodeByRowColumnIndex(pacMan.getY(), pacMan.getX(), gPane);
-                    pacMan.setXY(pacMan.getX()-1, pacMan.getY());
-                    break;
-                case RIGHT:
-                    gPane.add(pacMan, pacMan.getX()+1, pacMan.getY());
-                    break;
-            }
-        });
-        Scene scene = new Scene(pane);
+        Scene scene = new Scene(bPane);
         primaryStage.setTitle("PacMan");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
@@ -48,42 +39,26 @@ public class Main extends Application {
         for (int i=0; i<27;i++) {
             for (int j=0; j<21; j++) {
                 if (!map.accessMapArray()[i][j].getPassable()) {
-                    gPane.add(new Wall(), j, i);
+                    pane.add(new Wall(), j, i);
                 }
                 else if (map.accessMapArray()[i][j].getBigDot()) {
                     Dot bigDot = new Dot(true);
-                    gPane.add(bigDot, j, i);
-                    GridPane.setHalignment(bigDot, HPos.CENTER);
+                    pane.add(bigDot, j, i);
+                    Pane.setHalignment(bigDot, HPos.CENTER);
                 }
                 else if (map.accessMapArray()[i][j].getDot()) {
                     Dot dot = new Dot(false);
-                    gPane.add(dot, j, i);
-                    GridPane.setHalignment(dot, HPos.CENTER);
+                    pane.add(dot, j, i);
+                    Pane.setHalignment(dot, HPos.CENTER);
                 }
                 else if (map.accessMapArray()[i][j].getStarterArea()) {
                     pacMan.setXY(j, i);
-                    gPane.add(pacMan, pacMan.getX(), pacMan.getY());
+                    pane.add(pacMan, pacMan.getX(), pacMan.getY());
                     GridPane.setHalignment(pacMan, HPos.CENTER);
                 }
             }
         }
     }
-
-    public Node getNodeByRowColumnIndex (final int row, final int column, GridPane gridPane) {
-        Node result = null;
-        ObservableList<Node> childrens = gridPane.getChildren();
-
-        for (Node node : childrens) {
-            if(GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == column) {
-                result = node;
-                break;
-            }
-        }
-
-        return result;
-    }
-
-
 
     public static void main(String[] args) {
         launch(args);
