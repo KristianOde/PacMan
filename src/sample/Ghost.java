d61e2b601471ffd9ea8960868a30891962059154
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2020-02-27 12:43:31 +0100

    vulnerable, dead, dying states added

--- src/sample/Ghost.java
+++ src/sample/Ghost.java
@@ -4,10 +4,15 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
 
 public class Ghost extends Creature {
+
+    private boolean vulnerable= false;
+    private boolean dying= false;
+    private boolean dead= false;
+
     public Ghost() {
         setType(ArcType.OPEN);
-        setRadiusX(8);
-        setRadiusY(11);
+        setRadiusX(7);
+        setRadiusY(10);
         setStartAngle(310);
         setLength(280);
         setStroke(Color.BLACK);
@@ -16,8 +21,8 @@ public class Ghost extends Creature {
 
     public Ghost(Color color) {
         setType(ArcType.OPEN);
-        setRadiusX(8);
-        setRadiusY(11);
+        setRadiusX(7);
+        setRadiusY(10);
         setStartAngle(310);
         setLength(280);
         setStroke(Color.BLACK);
@@ -28,8 +33,8 @@ public class Ghost extends Creature {
         setCenterX(xpos);
         setCenterY(ypos);
         setType(ArcType.OPEN);
-        setRadiusX(8);
-        setRadiusY(11);
+        setRadiusX(7);
+        setRadiusY(10);
         setStartAngle(310);
         setLength(280);
         setStroke(Color.BLACK);
@@ -40,11 +45,44 @@ public class Ghost extends Creature {
         setCenterX(xpos);
         setCenterY(ypos);
         setType(ArcType.OPEN);
-        setRadiusX(8);
-        setRadiusY(11);
+        setRadiusX(7);
+        setRadiusY(10);
         setStartAngle(310);
         setLength(280);
         setStroke(Color.BLACK);
+        setFill(color);
+    }
+
+    /*get-set-is methods below*/
+    protected boolean isVulnerable() {
+        return vulnerable;
+    }
+
+    protected void setVulnerable(boolean trueOrFalse) {
+        this.vulnerable = trueOrFalse;
+    }
+
+    protected boolean isDying() {
+        return dying;
+    }
+
+    protected void setDying(boolean trueOrFalse) {
+        this.dying = trueOrFalse;
+    }
+
+    protected boolean isDead() {
+        return dead;
+    }
+
+    protected void setDead(boolean trueOrFalse) {
+        this.dead = trueOrFalse;
+    }
+
+    protected Color getColor() {
+        return (Color) getFill();
+    }
+
+    protected void setColor(Color color) {
         setFill(color);
     }
 }
